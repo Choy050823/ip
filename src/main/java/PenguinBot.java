f6fb75df447522e96a126a88353fafbdabd10ca5
Choy050823 <choymh23@gmail.com>
2026-01-23 14:10:25 +0800

    Level-3 Mark as done

--- src/main/java/PenguinBot.java
+++ src/main/java/PenguinBot.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 public class PenguinBot {
     public static void main(String[] args) {
-        String intialPrompt = "    ____________________________________________________________\n" +
+        String initialPrompt = "    ____________________________________________________________\n" +
                 "     Hello! I'm PenguinBot\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n";
 
-        System.out.println(intialPrompt);
+        System.out.println(initialPrompt);
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
 
         while (true) {
@@ -23,11 +23,17 @@ public class PenguinBot {
                             ____________________________________________________________\
                         """);
                 break;
+
             } else if (userInput.equals("list")){
                 StringBuilder output = new StringBuilder();
                 int i = 1;
                 while (i <= list.length && list[i - 1] != null) {
-                    output.append(i).append(". ").append(list[i - 1]).append("\n");
+                    output.append(i)
+                            .append(". [")
+                            .append(list[i - 1].getStatusIcon())
+                            .append("] ")
+                            .append(list[i - 1].description)
+                            .append("\n");
                     i++;
                 }
                 System.out.println("    ____________________________________________________________\n" +
@@ -35,11 +41,32 @@ public class PenguinBot {
                         "    ____________________________________________________________\n");
 
             } else {
-                list[count] = userInput;
-                count++;
-                System.out.println("    ____________________________________________________________\n" +
-                        "Added: " + userInput + "\n" +
-                        "    ____________________________________________________________\n");
+                String[] parts = userInput.split("\\s+", 2);
+                String action = parts[0];
+
+                if (action.equals("mark")) {
+                    int number = Integer.parseInt(parts[1]);
+
+                    list[number - 1].markAsDone();
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     Nice! I've marked this task as done:\n" +
+                            "       [" + list[number - 1].getStatusIcon() + "] " + list[number - 1].description + "\n" +
+                            "    ____________________________________________________________");
+                } else if (action.equals("unmark")){
+                    int number = Integer.parseInt(parts[1]);
+
+                    list[number - 1].markAsUndone();
+                    System.out.println("    ____________________________________________________________\n" +
+                            "     OK, I've marked this task as not done yet:\n" +
+                            "       [" + list[number - 1].getStatusIcon() + "] " + list[number - 1].description + "\n" +
+                            "    ____________________________________________________________");
+                } else {
+                    list[count] = new Task(userInput);
+                    count++;
+                    System.out.println("    ____________________________________________________________\n" +
+                            "Added: " + userInput + "\n" +
+                            "    ____________________________________________________________\n");
+                }
             }
         }
     }
