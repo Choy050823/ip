b1c619e44d2dad16275387366d076962fc6c0f87
Choy050823 <choymh23@gmail.com>
2026-01-23 12:16:05 +0800

    Level-0 Add greeting and rename file

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# PenguinBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/PenguinBot.java
@@ -0,0 +1,11 @@
+public class PenguinBot {
+    public static void main(String[] args) {
+        String prompt = "____________________________________________________________\n" +
+                " Hello! I'm PenguinBot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(prompt);
+    }
+}
