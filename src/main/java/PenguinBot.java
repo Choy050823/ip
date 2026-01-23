8fd2cfa6479cf2228a210aa4a64f1c92cab71bfc
Choy050823 <choymh23@gmail.com>
2026-01-23 12:38:05 +0800

    Level-1 Echo

--- src/main/java/PenguinBot.java
+++ src/main/java/PenguinBot.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class PenguinBot {
     public static void main(String[] args) {
-        String prompt = "____________________________________________________________\n" +
-                " Hello! I'm PenguinBot\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(prompt);
+        String intialPrompt = "    ____________________________________________________________\n" +
+                "     Hello! I'm [YOUR CHATBOT NAME]\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+
+        System.out.println(intialPrompt);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.next();
+            if (userInput.equals("bye")) {
+                System.out.println("""
+                            ____________________________________________________________
+                             Bye. Hope to see you again soon!
+                            ____________________________________________________________\
+                        """);
+                break;
+            } else {
+                System.out.println("    ____________________________________________________________\n" +
+                             userInput + "\n" +
+                        "    ____________________________________________________________\n");
+            }
+        }
+
     }
 }
