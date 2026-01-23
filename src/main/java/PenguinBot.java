2cbb43817185447aeff0f9e3d4fa85cf4ec73f54
Choy050823 <choymh23@gmail.com>
2026-01-23 12:48:59 +0800

    Level-2 Add List

--- src/main/java/PenguinBot.java
+++ src/main/java/PenguinBot.java
@@ -1,17 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PenguinBot {
     public static void main(String[] args) {
         String intialPrompt = "    ____________________________________________________________\n" +
-                "     Hello! I'm [YOUR CHATBOT NAME]\n" +
+                "     Hello! I'm PenguinBot\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n";
 
         System.out.println(intialPrompt);
 
+        String[] list = new String[100];
+        int count = 0;
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            String userInput = scanner.next();
+            String userInput = scanner.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("""
                             ____________________________________________________________
@@ -19,12 +23,24 @@ public class PenguinBot {
                             ____________________________________________________________\
                         """);
                 break;
-            } else {
+            } else if (userInput.equals("list")){
+                StringBuilder output = new StringBuilder();
+                int i = 1;
+                while (i <= list.length && list[i - 1] != null) {
+                    output.append(i).append(". ").append(list[i - 1]).append("\n");
+                    i++;
+                }
                 System.out.println("    ____________________________________________________________\n" +
-                             userInput + "\n" +
+                             output + "\n" +
+                        "    ____________________________________________________________\n");
+
+            } else {
+                list[count] = userInput;
+                count++;
+                System.out.println("    ____________________________________________________________\n" +
+                        "Added: " + userInput + "\n" +
                         "    ____________________________________________________________\n");
             }
         }
-
     }
 }
